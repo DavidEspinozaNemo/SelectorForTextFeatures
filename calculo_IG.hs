bc0b8277d7f3d4eca4a2156a82e4c48f50e09557
Unknown <david1998es16@gmail.com>
2019-09-24 16:27:52 -0600

    Pruebas

---
+++ calculo_IG.hs
@@ -0,0 +1,2 @@
+-- formula de calculo IG(ki)
+log2 x = if x == 0 then 0 else log(x)/log(2)
---
+++ test/test_1-2.hs
@@ -0,0 +1,14 @@
+import Prelude hiding (null, lookup, map, filter)
+import Data.HashMap.Lazy hiding (sort,map)
+import Data.Char
+import Data.List (sort,map)
+import System.IO
+
+incTupla3 :: (Integer, Integer, Integer) -> (Integer, Integer, Integer)
+incTupla3 (x, y, z) = (x + 1, y + 1, z + 1)
+
+--ConteoClases :: (Integer, Integer, Integer) -> Integer -> (Integer, Integer, Integer)
+ConteoClases :: (Int, Int, Int) -> Int -> (Int, Int, Int)
+ConteoClases (a,b,c) x | x<2 = (a+1, b, c)
+                       | x==2 = (a,b+1,c)
+                       | x>2 = (a,b,c+1)
